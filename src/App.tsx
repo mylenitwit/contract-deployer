3770eb5ad178e08d1367db6cadd219bd813e5104
mylenitwit <melihdincay@hotmail.com>
2025-03-18 05:53:16 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import ContractDeployer from "./ContractDeployer";
+
+function App() {
+  return (
+    <div className="App">
+      <ContractDeployer />
+    </div>
+  );
+}
+
+export default App;
